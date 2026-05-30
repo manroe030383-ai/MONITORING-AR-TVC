60b08626509fc7c7474ce18e1be9e6d64f0940e5
manroe030383-ai <manroe030383@gmail.com>
2026-05-30 11:19:16 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 // KONFIGURASI
-const SUPABASE_URL = 'https:ozcrikgzsadezarhccvp.supabase.co';
+const SUPABASE_URL = 'https://ozcrikgzsadezarhccvp.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96Y3Jpa2d6c2FkZXphcmhjY3ZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxMzQxOTgsImV4cCI6MjA4ODcxMDE5OH0.vSohadwQZV2SU4bjXfh-bPGZ1FV6ivo4e0irF10ITn8';
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
